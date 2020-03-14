ae8f95d04df92003974549c14676fc55b333dd0b
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-03-14 19:59:47 +0600

    expense

--- src/app/expense/expense.component.ts
+++ src/app/expense/expense.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import 'firebase/firestore';
+
 @Component({
   selector: 'app-expense',
   templateUrl: './expense.component.html',
@@ -8,17 +12,42 @@ import { FormControl } from '@angular/forms';
 })
 export class ExpenseComponent implements OnInit {
 
+  expenses:any;
   expenseArray = [];
+  expenseInput:any;
   today = new FormControl(new Date());
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) {
+
+  firestore.collection('Tally', ref => ref.orderBy('expense.datetime').limitToLast(5)).valueChanges().subscribe(object=> {
+
+
+      this.expenses = object;
+
+      console.log(object, 'object');
+
+     });
+
+  }
 
   ngOnInit(): void {
   }
+  getExpense(expense:number, expenseDate:any) {
+    var d = new Date().getTime().toString(); 
 
-  getExpense(expense:number) {
-    this.expenseArray.push(expense);
-    console.log(this.expenseArray);
+
+
+    this.firestore.collection('Tally').doc(d).set({
+        expense: {
+          amount: expense,
+          deposit_type: "savings",
+          datetime: d,
+          userdate: expenseDate
+        }
+
+    });
+
+    this.expenseInput = null;
 
   }
 
