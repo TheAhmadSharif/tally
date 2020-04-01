00896de0d94268eb8eefca4537c3f02df379aa76
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-04-01 11:48:15 +0600

    update UI

--- src/app/deposit/deposit.component.ts
+++ src/app/deposit/deposit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import 'firebase/firestore';
@@ -15,21 +17,60 @@ export class DepositComponent implements OnInit {
 
   depositArray = [];
   deposits:any;
+  today = new FormControl(new Date());
+  depositInput:any;
+  total: number = 0;
+  totalDeposit: number = 0;
 
-  constructor(firestore: AngularFirestore) {
-     firestore.collection('deposits').valueChanges().subscribe(object=> {
-      this.deposits = object;
 
-      console.log(object);
-     });
+
+  constructor(private firestore: AngularFirestore) {
+
+
   }
 
   ngOnInit(): void {
+   
+    
   }
 
-  getDeposit(deposit:number) {
-    this.depositArray.push(deposit);
-    console.log(this.depositArray);
+  getTotalDeposit() {
+
+        // this.firestore.collection("Tally").get().subscribe(object => {
+        //   console.log(object);
+        // });
+    
+
+        let citiesRef = this.firestore.collection('Tally');
+        let allCities = citiesRef.get().forEach(doc => {
+          console.log(doc);
+        });
+              
+
+
+  }
+
+  getDeposit(deposit:number, depositDate:any) {
+
+    var d = new Date().getTime().toString(); 
+
+
+
+    this.firestore.collection('Tally').doc(d).set({
+        deposit: {
+          amount: deposit,
+          deposit_type: "savings",
+          datetime: d,
+          userdate: depositDate
+        }
+
+    });
+
+    this.depositInput = null;
+
+    this.getTotalDeposit();
+
+
   }
 
 }
--- src/app/expense/expense.component.ts
+++ src/app/expense/expense.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-expense',
@@ -7,17 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseComponent implements OnInit {
 
+  expenses:any;
   expenseArray = [];
+  expenseInput:any;
+  today = new FormControl(new Date());
+  total:any = 0;
 
-  constructor() { }
+  @Output() getExpenseEvent = new EventEmitter();
+
+
+  constructor(private firestore: AngularFirestore) {
+
+  firestore.collection('Tally', ref => ref.orderBy('expense.datetime').limitToLast(5)).valueChanges().subscribe(object=> {
+
+
+
+        this.expenses = object;
+
+        for(var i = 0; i < this.expenses.length; i++) {
+            this.total = parseFloat(this.expenses[i].expense.amount) + this.total;
+        }
+
+
+
+
+     });
+
+  }
 
   ngOnInit(): void {
   }
+  getExpense(expense:number, expenseDate:any) {
+    var d = new Date().getTime().toString(); 
 
-  getExpense(expense:number) {
-    this.expenseArray.push(expense);
-    console.log(this.expenseArray);
 
+
+    this.firestore.collection('Tally').doc(d).set({
+        expense: {
+          amount: expense,
+          deposit_type: "savings",
+          datetime: d,
+          userdate: expenseDate
+        }
+
+    });
+
+    this.expenseInput = null;
+
+  }
+
+  getTotalExpense() {
+    this.getExpenseEvent.emit('event');
   }
 
 }
