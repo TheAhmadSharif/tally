98988b17fe6c13ce439f131cd3a0603817ca5707
theahmadsharif <theahmadsharif@gmail.com>
2020-04-09 02:00:20 +0600

    deatils page

--- src/app/expense-details/expense-details.component.ts
+++ src/app/expense-details/expense-details.component.ts
@@ -20,17 +20,13 @@ export class ExpenseDetailsComponent implements OnInit {
     this.first_parameter = this.route.snapshot.paramMap.get('type');
     this.second_parameter = this.route.snapshot.paramMap.get('object');
     
-    console.log(this.first_parameter, 'first_parameter');
-    console.log(this.second_parameter, 'second_parameter');
-
 
   }
 
   ngOnInit(): void {
-          this.firestore.collection('Tally').doc(this.second_parameter).valueChanges().subscribe(object=> {
+          var id = this.second_parameter; 
+          this.firestore.collection('Tally').doc(id).valueChanges().subscribe(object=> {
             this.object = object;
-            console.log(this.object, 'this.expense');
-            
         }, error => {
 
         });
