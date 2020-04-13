f84179bb399956b8ed2692d37e0dfc7c01eb56f0
Ahmad Sharif <TheAhmadSharif@gmail.com>
2020-04-13 12:34:47 +0600

    sidebar switch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Input } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import 'firebase/firestore';
 import 'lodash';
@@ -17,13 +17,23 @@ export class AppComponent implements OnInit {
   isCollapsed:boolean = true;
   calculateBox:any = 0;
   output:any = 'Result';
+  switch:any = 'off';
 
-  constructor(private firestore: AngularFirestore) {
+  constructor(private firestore: AngularFirestore, private elementRef: ElementRef) {
   }
 
   ngOnInit(): void {
-   //  console.log(_.chunk(['a', 'b', 'c', 'd'], 2)); //lodash function
+   // console.log(_.chunk(['a', 'b', 'c', 'd'], 2)); //lodash function
    // console.log(_.random(1, 100)); //lodash function
+  }
+  ngAfterViewInit() {
+    // Otherwise Angular throws error: Expression has changed after it was checked.
+    window.setTimeout(() => {
+        this.elementRef.nativeElement.focus();
+    });
+  }
+  navSwitch(){
+    this.switch = 'on';
   }
 
   getTotalExpense (event) {
@@ -42,8 +52,11 @@ export class AppComponent implements OnInit {
   }
 
   addToDisplay(item:any) {
-    this.calculateBox =  this.calculateBox + item;
-    console.log(item);
+    var s = item;
+    s = s.replace(/^0+/, "");
+
+    this.calculateBox =  item;
+    console.log(this.calculateBox, '55');
   }
   getResult(equation:any) {
     this.output = eval(equation);
